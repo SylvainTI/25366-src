6cfaaeb78c2ab7b3838d3568d770b8b972098320
poinot.thierry <poinot.thierry@19eb5690-8e38-11dd-a14b-9ba93c563a2f>
2008-11-20 12:10:01 +0000

    liste des différentes actions : a faire les XML générés

--- data/index.php
+++ data/index.php
@@ -6,7 +6,33 @@ header('Content-type: application/xml; charset="utf-8"');
 $action=isset($_GET["action"]) ? $_GET['action'] : false;
 $langSource=isset($_GET["langSource"]) ? $_GET['langSource'] : false;
 switch ($action) {
-	case "theme":
+	case "langs":
+		echo "
+<langs>
+	<lang>
+		<id>fr-fra</id>
+		<lib><![CDATA[français]]></lib>
+		<destinations>
+			<lang>
+				<id>zh-zho</id>
+				<lib><![CDATA[Chinois]]></lib>
+			</lang>
+		</destinations>
+	</lang>
+	<lang>
+		<id>zh-zho</id>
+		<lib><![CDATA['chinois' en chinois]]></lib>
+		<destinations>
+			<lang>
+				<id>zh-zho</id>
+				<lib><![CDATA['français' en chinois]]></lib>
+			</lang>
+		</destinations>
+	</lang>
+</langs>
+		";
+		break;
+	case "themes":
 		echo "
 <themes>
 	<theme>
@@ -27,5 +53,45 @@ switch ($action) {
 </themes>
 		";
 		break;
+	
+	case "subthemes":
+	//todo
+		echo "
+<subthemes>
+	<subtheme>
+		<id>0</id>
+		<lang>$langSource</lang>
+		<lib><![CDATA[naissance]]></lib>
+	</subtheme>
+	<subtheme>
+		<id>1</id>
+		<lang>$langSource</lang>
+		<lib><![CDATA[alimentation]]></lib>
+	</subtheme>
+	<subtheme>
+		<id>2</id>
+		<lang>$langSource</lang>
+		<lib><![CDATA[espace]]></lib>
+	</subtheme>
+</subthemes>
+		";
+		break;
+	case "words":
+	//todo
+		echo "
+<words>
+</subthemes>
+		";
+		break;
+	case "word":
+	//todo
+		break;
+	case "historical":
+	//todo
+		break;
+	case "search":
+	//todo
+		break;
+
 }
 ?>
