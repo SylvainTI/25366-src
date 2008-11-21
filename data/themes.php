2a85cc02988a92f3b932f32b2db2acc3f38e693d
poinot.thierry <poinot.thierry@19eb5690-8e38-11dd-a14b-9ba93c563a2f>
2008-11-21 20:00:07 +0000

    génération du XML de listage des thèmes opérationnelles

---
+++ data/themes.php
@@ -0,0 +1,18 @@
+<themes>
+	<theme>
+<?php
+	$themeId = 1;
+	$lib = "Vie";
+?>		<id><?php echo $themeId; ?></id>
+		<lang><?php echo $langSource; ?></lang>
+		<lib><![CDATA[<?php echo $lib; ?>]]></lib>
+	</theme>
+	<theme>
+<?php
+	$themeId = 2;
+	$lib = "Voyages";
+?>		<id><?php echo $themeId; ?></id>
+		<lang><?php echo $langSource; ?></lang>
+		<lib><![CDATA[<?php echo $lib; ?>]]></lib>
+	</theme>
+</themes>
