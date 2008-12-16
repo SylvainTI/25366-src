740bdbd383dcd1064c27df4cd23f070419ac9d37
poinot.thierry <poinot.thierry@19eb5690-8e38-11dd-a14b-9ba93c563a2f>
2008-12-16 10:26:34 +0000

    unicode/index.php: insertion themes dans la base de données db.sql : export de la base avec les themes actions/themes.php : génération du xml avec la base de données

--- flex/bin-debug/data/db.sql
+++ flex/bin-debug/data/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Serveur: localhost
--- Généré le : Mar 16 Décembre 2008 à 09:36
+-- Généré le : Mar 16 Décembre 2008 à 11:23
 -- Version du serveur: 5.0.45
 -- Version de PHP: 5.2.5
 
@@ -19,6 +19,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Structure de la table `lang_dest`
 -- 
 
+DROP TABLE IF EXISTS `lang_dest`;
 CREATE TABLE IF NOT EXISTS `lang_dest` (
   `lang_source_id` varchar(7) NOT NULL,
   `lang_id` varchar(7) NOT NULL,
@@ -38,6 +39,7 @@ INSERT INTO `lang_dest` (`lang_source_id`, `lang_id`, `lib_source`) VALUES
 -- Structure de la table `lang_source`
 -- 
 
+DROP TABLE IF EXISTS `lang_source`;
 CREATE TABLE IF NOT EXISTS `lang_source` (
   `id` varchar(7) NOT NULL,
   `lib` varchar(50) NOT NULL
@@ -56,6 +58,7 @@ INSERT INTO `lang_source` (`id`, `lib`) VALUES
 -- Structure de la table `subtheme`
 -- 
 
+DROP TABLE IF EXISTS `subtheme`;
 CREATE TABLE IF NOT EXISTS `subtheme` (
   `id` int(11) NOT NULL,
   `lang` varchar(10) NOT NULL,
@@ -74,16 +77,47 @@ CREATE TABLE IF NOT EXISTS `subtheme` (
 -- Structure de la table `theme`
 -- 
 
+DROP TABLE IF EXISTS `theme`;
 CREATE TABLE IF NOT EXISTS `theme` (
   `id` int(11) NOT NULL,
   `lang` varchar(10) NOT NULL,
-  `lib` varchar(60) NOT NULL
+  `lib` varchar(60) NOT NULL,
+  `image` varchar(50) NOT NULL default 'key.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
 -- Contenu de la table `theme`
 -- 
 
+INSERT INTO `theme` (`id`, `lang`, `lib`, `image`) VALUES 
+(1, 'fr-fra', 'vie', 'vie.png'),
+(2, 'fr-fra', 'alimentation', 'alimentation.png'),
+(3, 'fr-fra', 'jeu & sport', 'jeu.png'),
+(4, 'fr-fra', 'société', 'societe.png'),
+(5, 'fr-fra', 'art', 'art.png'),
+(6, 'fr-fra', 'commerce', 'commerce.png'),
+(7, 'fr-fra', 'faire', 'faire.png'),
+(8, 'fr-fra', 'communication', 'communication.png'),
+(9, 'fr-fra', 'science', 'science.png'),
+(10, 'fr-fra', 'intellect', 'intellect.png'),
+(11, 'fr-fra', 'aller', 'aller.png'),
+(13, 'fr-fra', 'nombre', 'nombre.png'),
+(14, 'fr-fra', 'édifice', 'edifice.png'),
+(15, 'fr-fra', 'corps', 'corps.png'),
+(16, 'fr-fra', 'sens', 'sens.png'),
+(17, 'fr-fra', 'végétaux & animaux', 'vegetaux.png'),
+(18, 'fr-fra', 'manger', 'manger.png'),
+(19, 'fr-fra', 'pouvoir', 'pouvoir.png'),
+(20, 'fr-fra', 'militaire', 'militaire.png'),
+(21, 'fr-fra', 'famille', 'famille.png'),
+(22, 'fr-fra', 'matière', 'matiere.png'),
+(23, 'fr-fra', 'espace', 'espace.png'),
+(24, 'fr-fra', 'objet', 'objet.png'),
+(25, 'fr-fra', 'temps', 'temps.png'),
+(26, 'fr-fra', 'sentiment', 'sentiment.png'),
+(27, 'fr-fra', 'homme', 'homme.png'),
+(28, 'fr-fra', 'textile', 'textile.png'),
+(29, 'fr-fra', 'arts', 'arts.png');
 
 -- --------------------------------------------------------
 
@@ -91,6 +125,7 @@ CREATE TABLE IF NOT EXISTS `theme` (
 -- Structure de la table `word`
 -- 
 
+DROP TABLE IF EXISTS `word`;
 CREATE TABLE IF NOT EXISTS `word` (
   `id` int(11) NOT NULL,
   `lang` varchar(10) NOT NULL,
