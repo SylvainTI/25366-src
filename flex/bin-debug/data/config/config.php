4722df9a21aafc93a61bf2f40e60423b48f77ec7
florian.tamarelle <florian.tamarelle@19eb5690-8e38-11dd-a14b-9ba93c563a2f>
2008-12-18 01:21:13 +0000

    :-)

--- flex/bin-debug/data/config/config.php
+++ flex/bin-debug/data/config/config.php
@@ -1,7 +1,7 @@
 <?php
 ## Initialisation de la base de données (singleton PDO)
 require_once(dirname(__FILE__).'/../lib/DatabaseManager.class.php');
-$db = DatabaseManager::getInstance('mysql:dbname=25366src;host=localhost', 'root', 'root', 'utf8');
+$db = DatabaseManager::getInstance('mysql:dbname=25366src;host=localhost', 'root', '', 'utf8');
 
 if (file_exists('INSTALL')) {
 	$sql = file_get_contents('db.sql');
