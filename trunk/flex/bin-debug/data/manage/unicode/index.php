24ba73f86b092fe183c8fa41532938d3d9f1ef13
poinot.thierry <poinot.thierry@19eb5690-8e38-11dd-a14b-9ba93c563a2f>
2008-12-24 14:06:39 +0000

    optimisation du code de chargement du XLS dans la BDD + conserve l'intégrité des mots - thèmes - sous-thèmes
    le nouveau DUMP SQL arrive sous peu :P

--- trunk/flex/bin-debug/data/manage/unicode/index.php
+++ trunk/flex/bin-debug/data/manage/unicode/index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /* 
 Thierry, c'est une lecture du fichier data_unicode.txt.
 Les caractères chinois ne se voient pas avec notepad++ (probablement un souci de substitution typo)
@@ -8,103 +8,65 @@ Sans ça, rien ne marche bien
 Arnaud
 */
 header('Content-type: text/html; charset=UTF-8');
-$data = file_get_contents("data_unicode.txt");
-$data = mb_convert_encoding( $data, 'UTF-8', 'UTF-16LE'); // Excel fait de l'utf 16 little endian, on convertit en utf-8
-$lines = explode("\r\n",$data);
+$file = file_get_contents("data_unicode.txt");
+$file = mb_convert_encoding( $file, 'UTF-8', 'UTF-16LE'); // Excel fait de l'utf 16 little endian, on convertit en utf-8
+$lines = explode("\n",$file);
 $themes = array();
 $subthemes = array();
 $words = array();
 $words_list = array();
 $subthemes_list = array();
-echo "<table>";
-foreach ($lines as $line){
-	$values = explode("	",$line);
-/*
-	echo "<tr>";
-	foreach ($values as $value){
-		echo "<td>".$value."</td>";
-	}
-	echo "</tr>";
-//*/
-	// extraction des thèmes
-	if (!in_array($values[0], $themes)) {
-		$themes[] = $values[0];
-	}
 
-}
-echo "</table>";
-$i=0;
-//*
-foreach ($lines as $line) {
-	$data = explode("	", $line);
-	$i++;
+$i = $j = $k = 0;
+require_once('../../config/config.php');
+
+foreach ($lines as $nb_line => $line){
+	$data = explode("	",$line);
+	##  extraction des thèmes
+	if (!in_array($data[0], $themes)) {
+		$i++;
+		$themes[$i] = $data[0];
+		//$db->query("INSERT INTO `theme` (`id`, `lib`, `lang`) VALUES ('$i','{$themes[$i]}','fr-fra')");		
+	}
 	if (!in_array($data[1], $subthemes_list)) {
-		$subthemes_list[] = $data[1];
-		//$subthemes[$data[1]] = $i;
-		$subthemes[$i]['subtheme'] = $data[1];
-		$subthemes[$i]['theme'] = $data[0];
-		$subthemes[$i]['chinois'] = $data[5];
+		$j++;
+		$subthemes_list[$j] = $data[1];
+		$subthemes[$j]['subtheme'] = $data[1];
+		$subthemes[$j]['theme'] = $data[0];
+		$subthemes[$j]['chinois'] = $data[5];
 		// récupération de la clé du thème pour le sous-thèmes
-		foreach ($themes as $key => $theme) {
-			if ($data[0] ==  $theme) {
-				$subthemes[$i]['theme_id'] = $key;
-			}
-		}
+		$subthemes[$j]['theme_id'] = $i;
+		//$db->query("INSERT INTO `subtheme` (`id`, `lib`, `lang`, `idTheme`) VALUES ('$j','{$subthemes[$j]['subtheme']}','fr-fra', '{$subthemes[$j]['theme_id']}')");		
 	}
-}
-//*/
-//*
-$i=0;
-// extraction des mots
-echo count($subthemes).'<br/>';
-foreach ($lines as $line) {
-	$data = explode("	", $line);
-	$i++;
 	if (!in_array($data[2], $words_list)) {
-		$words_list[] = $data[2];
-		$words[$i]['subtheme'] = $data[1];
-		$words[$i]['theme'] = $data[0];
-		$words[$i]['word'] = $data[2];
-		$words[$i]['genre'] = $data[4];
-		$words[$i]['chinois'] = $data[5];
-		$words[$i]['phrase'] = $data[6];
-		$words[$i]['phrase_c'] = $data[7];
-		//$words[$i]['subtheme_id'] = $subthemes[$data[1]];
+		$k++;
+		$words_list[$k] = $data[2];
+		$words[$k]['subtheme'] = $data[1];
+		$words[$k]['theme'] = $data[0];
+		$words[$k]['word'] = $data[2];
+		$words[$k]['genre'] = $data[4];
+		$words[$k]['chinois'] = $data[5];
+		$words[$k]['phrase'] = $data[6];
+		$words[$k]['phrase_c'] = $data[7];
+		$words[$k]['tags'] = $data[8];
 		// récupération de la clé du sous-themes pour le mot
-		//*
-		$j=0;
-		foreach ($subthemes as $subtheme) {
-			$j++;
-			//echo count($subthemes).'<br/>';
-			//echo ($data[1].' = '.$subtheme['subtheme']."<br/>\n");
-			if ($data[1] ==  $subtheme['subtheme']) {
-				//echo $subthemes[$subtheme['subtheme']].'<br/>';
-				$words[$i]['subtheme_id'] = $j;
-				break;
-
-			}
-		}
-		//*/
+		$words[$k]['subtheme_id'] = $j;
+		//$db->query("INSERT INTO `word` (`id`, `lib`, `lang`, `definition`, `genre`, `idSubtheme`, `tags`) VALUES ('$k','{$words[$k]['chinois']}','zh-zho', '{$words[$k]['phrase_c']}', '', '{$words[$k]['subtheme_id']}', '')");
+		//$db->query("INSERT INTO `word` (`id`, `lib`, `lang`, `definition`, `genre`, `idSubtheme`, `tags`) VALUES ('$k','{$words[$k]['word']}','fr-fra', '{$words[$k]['phrase']}', '{$words[$k]['genre']}', '{$words[$k]['subtheme_id']}', '{$words[$k]['tags']}')");
 	}
+	echo "$i - $j - $k - {$themes[$i]} - {$subthemes[$j]['subtheme']} - {$words[$k]['word']}<br/>";
 }
 $i=0;
 //*/
-require_once('../config/config.php');
-sort($themes);
-sort($subthemes);
-sort($words);
-/* insertion des mots dans la base
-foreach ($words as $key => $word) {
-$i++;
-			$db->query("INSERT INTO `word` (`id`, `lib`, `lang`, `definition`, `genre`, `idSubtheme`) VALUES ('$i','{$words[$key]['chinois']}','zh-zho', '{$words[$key]['phrase_c']}', '', '{$words[$key]['subtheme_id']}')");
-			//echo("INSERT INTO `word` (`id`, `lib`, `lang`, `definition`, `genre`, `idSubtheme`) VALUES ('$i','{$words[$key]['word']}','fr-fra', '{$words[$key]['phrase']}', '{$words[$key]['genre']}', '{$words[$key]['subtheme_id']}')".'<br/>');
-}
+//sort($themes);
+//sort($subthemes);
+//sort($words);
+
 //*/
 
 /* Insertion des sous-thèmes dans la base
 	foreach ($subthemes as $key => $subtheme) {
-			$db->query("INSERT INTO `subtheme` (`id`, `lib`, `lang`, `idTheme`) VALUES ('$key','{$subthemes[$key]['subtheme']}','fr-fra', '{$subthemes[$key]['theme_id']}')");
-		
+			$db->query("INSERT INTO `subtheme` (`id`, `lib`, `lang`, `idTheme`) VALUES ('$key','{$subthemes[$key]['subtheme']}','fr-fra', '{$subthemes[$key]['theme_id']}')");		
 	}
 //*/
 
@@ -123,6 +85,6 @@ $i++;
 	//echo count($lines);
 	//echo (print_r($themes, true));
 	//echo (print_r($subthemes, true));
-	echo (print_r($words, true));
+	//echo (print_r($words, true));
 
 ?>
