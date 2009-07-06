9fb22e7a44b02eff83f2ac1dc0583b8a375f116d
poinot.thierry <poinot.thierry@19eb5690-8e38-11dd-a14b-9ba93c563a2f>
2009-07-06 21:45:05 +0000

    ajout du swif des clés vectorielles

--- trunk/src/data/actions/themes.php
+++ trunk/src/data/actions/themes.php
@@ -1,18 +1,19 @@
 <themes>
 <?php
 	$db = DatabaseManager::getInstance();
-	$queryThemes = $db->prepare("SELECT * FROM `theme` WHERE `lang` = ? ORDER BY `id` ASC"); // préparation de la requête
+	$queryThemes = $db->prepare("SELECT * FROM `theme2` WHERE `lang` = ? ORDER BY `id` ASC"); // préparation de la requête
 	$queryThemes->execute(array($langSource)); // on exécute la requête
 	$themes = $queryThemes->fetchAll();
 	foreach ($themes as $theme) :
-?>
-		
+?>	
 	<theme>
 		<id><?php echo $theme['id']; ?></id>
 		<lang><?php echo $theme['lang']; ?></lang>
 		<lib><![CDATA[<?php echo $theme['lib']; ?>]]></lib>
 		<image><?php echo $theme['image']; ?></image>
+		<asset><?php echo $theme['asset']; ?></asset>
 	</theme>
+
 <?php
 	endforeach;
 ?>
